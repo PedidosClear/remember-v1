fd31ddcbe8f1519c396a6d215dd723293f4912dd
PedidosClear <62444106+PedidosClear@users.noreply.github.com>
2020-05-01 16:47:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -32,12 +32,22 @@ app.get('/pending', function(req, res){
 
 app.use('/api', require('./routes/mp'));
 
-const oneHour = 60*60*24*7;
+var options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html','js','css','jpeg','png','ico'],
+    index: false,
+    maxAge: '7d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
+  }
 
 // Middleware para vue js modo history
 const history =require('connect-history-api-fallback')
 app.use(history()) // antes de ruta estatica
-app.use(express.static(path.join(__dirname, 'public'),{ maxAge: oneHour }))
+app.use(express.static(path.join(__dirname, 'public'),options))
 
 app.set('puerto', process.env.PORT || 5000);
 app.listen(app.get('puerto'),function(){
